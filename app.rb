48bec3e5596e46e57de22bead72345ba5456f326
Debbie Cravey & Alister A. Fenix <student@epicodus.com>
2015-01-20 14:48:20 -0800

    Add find & task methods; update Sinatra files to show tasks within lists.

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-equire("sinatra")
+require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/task")
@@ -18,4 +18,18 @@ post("/lists") do
   list.save()
   @lists = List.all()
   erb(:index)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  task = Task.new({:description => description, :list_id => list_id})
+  task.save()
+  @list = List.find(list_id)
+  erb(:list)
 end
